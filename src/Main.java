9d97660de09967ce1420c0d5a004c1a63881de96
clem <Moscow.phoenix@yandex.ru>
2024-08-14 17:19:17 +0300

    correcting the code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 1000;
+        int miles = 20;
+
+        System.out.println("начисленно мили:");
+        System.out.println(ticket / miles);
+
+    }
 }
+
