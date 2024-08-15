0e0dedf9aec9a71277a2171a798224395e41a5c7
clem <Moscow.phoenix@yandex.ru>
2024-08-15 15:08:29 +0300

    adjustment bonus calculation

--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Main2 {
+
+    public static void main(String[] args) {
+
+        int initialAmount = 100;
+        int investment = 1100;
+        int bonus;
+        if (investment > 1000) {
+            bonus = investment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+
+    }
 }
